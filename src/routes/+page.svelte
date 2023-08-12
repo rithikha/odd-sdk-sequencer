5b2b9f3b1d29afcff9bb8b17fb80f3b98125a5b3
rithikha <rithikha.rajamohan@gmail.com>
2023-08-12 17:01:01 -0400

    Initialize avivash/add-height-to-alpha-tag

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { sessionStore } from '../stores'
+  import Authed from '$components/home/Authed.svelte'
+  import Public from '$components/home/Public.svelte'
+</script>
+
+{#if $sessionStore?.authed}
+  <Authed />
+{:else}
+  <Public />
+{/if}
